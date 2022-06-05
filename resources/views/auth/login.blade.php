9d97c9acb26b4a3f469badaa19b1b39fd412b8f6
Firdaus J <101040362+firdausjamaludin@users.noreply.github.com>
2022-06-06 00:15:11 +0800

    update login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,21 @@
-<div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-    <a href="/login" class="text-sm text-gray-700 dark:text-gray-500">
-        <x-jet-button class="ml-4">
-            {{ __('Log in') }}
-        </x-jet-button>
-    </a>
-    <a href="/register" class=" text-sm text-gray-700 dark:text-gray-500 ">
-        <x-jet-button class="ml-4">
-            {{ __('Register') }}
-        </x-jet-button>
-    </a>
-</div>
-
 <x-guest-layout>
+    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+        <a href="/login" class="text-sm text-gray-700 dark:text-gray-500">
+            <x-jet-button class="ml-4">
+                {{ __('Log in') }}
+            </x-jet-button>
+        </a>
+        <a href="/register" class=" text-sm text-gray-700 dark:text-gray-500 ">
+            <x-jet-button class="ml-4">
+                {{ __('Register') }}
+            </x-jet-button>
+        </a>
+    </div>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
         </x-slot>
-
         <x-jet-validation-errors class="mb-4" />
-
         @if (session('status'))
         <div class="mb-4 font-medium text-sm text-green-600">
             {{ session('status') }}
@@ -52,10 +49,16 @@
                 </a>
                 @endif
 
+                <a class="ml-4 underline text-sm text-gray-600 hover:text-gray-900" href="/register">
+                    {{ __('Register') }}
+                </a>
+
                 <x-jet-button class="ml-4">
                     {{ __('Log in') }}
                 </x-jet-button>
             </div>
         </form>
+
     </x-jet-authentication-card>
+
 </x-guest-layout>
