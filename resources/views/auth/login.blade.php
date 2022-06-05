c4a48b7428be87a94a4120e13922a539814b24df
Firdaus J <101040362+firdausjamaludin@users.noreply.github.com>
2022-06-05 23:55:01 +0800

    Push

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,14 @@
 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-    <a href="/login" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-    <a href="/register" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+    <a href="/login" class="text-sm text-gray-700 dark:text-gray-500">
+        <x-jet-button class="ml-4">
+            {{ __('Log in') }}
+        </x-jet-button>
+    </a>
+    <a href="/register" class=" text-sm text-gray-700 dark:text-gray-500 ">
+        <x-jet-button class="ml-4">
+            {{ __('Register') }}
+        </x-jet-button>
+    </a>
 </div>
 
 <x-guest-layout>
